d741e11f0f746a8387a6610d2268ebe0cba8d0a9
brok3n <limesaru@gmail.com>
2023-06-17 18:33:59 +0900

    UI

--- Escape/game.c
+++ Escape/game.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h>
+#include "defines.h"
 #pragma warning(disable:4996)
+
+int gBoardHeight;
+int gBoardWidth;
 
 
 //SYSTEM
 COORD GetCurrentCursorPos();
 void SetCurrentCursorPos(int x, int y);
+
+//DRAW
+void ControlConsoleWindow();
+int ShowMainMenu();
+int ShowGame();
+void ShowResult();
+void DrawGameUI();
+void UpdateTimeUI(int time);
+void UpdateGameUI();
+int GamePause();
+void ShowGameOver();
+void ShowStageClear(int time);
+void ClearConsole();
 
 
 int main()
@@ -38,3 +55,367 @@ void SetCurrentCursorPos(int x, int y)
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+
+
+//DRAW
+void ControlConsoleWindow()
+{
+	CONSOLE_CURSOR_INFO curInfo;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curInfo.bVisible = 0;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+
+	system("mode con cols=236 lines=65 | title Escape From Sejong");
+}
+int ShowMainMenu()
+{
+	int select = 1;
+	//1 = 게임시작 / 0 = 게임종료
+
+	SetCurrentCursorPos(70, 10);
+	printf(" #######   #####     ####     ##     ######   #######           #######  ######     ###    ##   ## ");
+	SetCurrentCursorPos(70, 11);
+	printf(" ##       ##   ##   ##  ##    ##     ##   ##  ##                ##       ##   ##   ## ##   ##   ## ");
+	SetCurrentCursorPos(70, 12);
+	printf(" ##       ##       ##        ####    ##   ##  ##                ##       ##   ##  ##   ##  ### ### ");
+	SetCurrentCursorPos(70, 13);
+	printf(" #####     #####   ##        ## #    ######   #####             #####    ######   ##   ##  ## # ## ");
+	SetCurrentCursorPos(70, 14);
+	printf(" ##            ##  ##       ######   ##       ##                ##       ## ##    ##   ##  ## # ## ");
+	SetCurrentCursorPos(70, 15);
+	printf(" ##       ##   ##   ##  ##  ##   #   ##       ##                ##       ##  ##    ## ##   ##   ## ");
+	SetCurrentCursorPos(70, 16);
+	printf(" #######   #####     ####  ###   ##  ##       #######           ##       ##   ##    ###    ##   ## ");
+
+	SetCurrentCursorPos(91, 19);
+	printf("  #####   #######    ####     ###    ##   ##    ####  ");
+	SetCurrentCursorPos(91, 20);
+	printf(" ##   ##  ##           ##    ## ##   ###  ##   ##  ## ");
+	SetCurrentCursorPos(91, 21);
+	printf(" ##       ##           ##   ##   ##  ###  ##  ##      ");
+	SetCurrentCursorPos(91, 22);
+	printf("  #####   #####        ##   ##   ##  ## # ##  ##      ");
+	SetCurrentCursorPos(91, 23);
+	printf("      ##  ##       ##  ##   ##   ##  ## # ##  ##  ### ");
+	SetCurrentCursorPos(91, 24);
+	printf(" ##   ##  ##       ##  ##    ## ##   ##  ###   ##  ## ");
+	SetCurrentCursorPos(91, 25);
+	printf("  #####   #######   ####      ###    ##   ##    ##### ");
+
+	SetCurrentCursorPos(115, 40);
+	printf("GAME START");
+
+	SetCurrentCursorPos(118, 44);
+	printf("EXIT");
+
+	SetCurrentCursorPos(108, 40);
+	printf("▶");
+
+	while (1)
+	{
+		if (_kbhit() != 0)
+		{
+			int key = _getch();
+			//printf("%d", key);
+
+			switch (key)
+			{
+			case DARROW:
+				if (select == 1)
+				{
+					select = 0;
+					SetCurrentCursorPos(108, 40);
+					printf("  ");
+					SetCurrentCursorPos(111, 44);
+					printf("▶");
+				}
+				break;
+			case UARROW:
+				if (select == 0)
+				{
+					select = 1;
+					SetCurrentCursorPos(111, 44);
+					printf("  ");
+					SetCurrentCursorPos(108, 40);
+					printf("▶");
+				}
+				break;
+			case ENTER:
+				ClearConsole();
+				return select;
+				break;
+			}
+		}
+	}
+}
+int ShowGame()
+{
+	
+}
+void ShowResult()
+{
+
+}
+void DrawGameUI()
+{
+	int statusBoardWidth = 28;
+	int statusBoardHeight = gBoardHeight;
+
+	{
+		for (int y = 0; y < gBoardHeight + 2; y += gBoardHeight + 1)
+			for (int x = 2; x < (gBoardWidth * 2) + statusBoardWidth + 2; x += 2)
+			{
+				SetCurrentCursorPos(x, y);
+				printf("─");
+			}
+
+		for (int x = 0; x < (gBoardWidth * 2) + 3; x += (gBoardWidth * 2) + 2)
+			for (int y = 1; y < gBoardHeight + 1; y++)
+			{
+				SetCurrentCursorPos(x, y);
+				printf("│");
+			}
+
+		for (int y = 1; y < gBoardHeight + 1; y++)
+		{
+			SetCurrentCursorPos((gBoardWidth * 2) + statusBoardWidth + 2, y);
+			printf("│");
+		}
+	}
+
+	// 유저블록판그리기
+	{
+		int x, y;
+		x = (gBoardWidth * 2) + 6, y = gBoardHeight - 1;
+		for (int z = 1; z <= 3; z++)
+		{
+			for (int x = (gBoardWidth * 2) + 2; x <= (gBoardWidth * 2) + 30; x += 2)
+			{
+				SetCurrentCursorPos(x, y);
+				if (x == (gBoardWidth * 2) + 2)
+					printf("├");
+				else if (x == (gBoardWidth * 2) + 30)
+					printf("┤");
+				else
+					printf("─");
+			}
+			y -= 6;
+		}
+		for (int y = gBoardHeight - 1; y >= gBoardHeight - 13; y--)
+		{
+			x = (gBoardWidth * 2) + 16;
+			SetCurrentCursorPos(x, y);
+			if (y == gBoardHeight - 13)
+				printf("┬");
+			else if (y == gBoardHeight - 1)
+				printf("┴");
+			else if (y == gBoardHeight - 7)
+				printf("┼");
+			else
+				printf("│");
+
+		}
+	}
+
+	{
+		SetCurrentCursorPos(0, 0);
+		printf("┌");
+		SetCurrentCursorPos((gBoardWidth * 2) + 2, gBoardHeight + 1);
+		printf("┴");
+		SetCurrentCursorPos((gBoardWidth * 2) + 2, 0);
+		printf("┬");
+		SetCurrentCursorPos(0, gBoardHeight + 1);
+		printf("└");
+		SetCurrentCursorPos((gBoardWidth * 2) + statusBoardWidth + 2, 0);
+		printf("┐");
+		SetCurrentCursorPos((gBoardWidth * 2) + statusBoardWidth + 2, gBoardHeight + 1);
+		printf("┘");
+	}
+
+	SetCurrentCursorPos((gBoardWidth * 2) + (statusBoardWidth / 2), 2);
+	printf("TIME");
+	SetCurrentCursorPos((gBoardWidth * 2) + (statusBoardWidth / 2), 3);
+	printf("%02d:%02d", 0, 0);
+	SetCurrentCursorPos((gBoardWidth * 2) + (statusBoardWidth / 2), 5);
+	//for (int i = 1; i <= 3; i++)
+	//{
+	//	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), YELLOW);
+	//	if (i <= SideQuest)
+	//		printf("★");
+	//}
+	//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE);
+
+	//if (p.isExtraLife)
+	//{
+	//	SetCurrentCursorPos((gBoardWidth * 2) + (statusBoardWidth / 2) + 2, 7);
+	//	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), RED);
+	//	printf("♥ ");
+	//}
+}
+void UpdateTimeUI(int time)
+{
+	
+}
+void UpdateGameUI()
+{
+	
+}
+int GamePause()
+{
+	//0 = 메인 메뉴로 / 1 = 게임 재개
+
+	int select = 1;
+
+	for (int y = gBoardHeight / 2 - 6; y <= gBoardHeight / 2 + 6; y++)
+	{
+		for (int x = (gBoardWidth * 2) / 2 - 15; x <= (gBoardWidth * 2) / 2 + 15; x++)
+		{
+			SetCurrentCursorPos(x, y);
+			printf("  ");
+		}
+	}
+
+	for (int y = gBoardHeight / 2 - 5; y <= gBoardHeight / 2 + 5; y += 10)
+	{
+		for (int x = gBoardWidth - 15; x <= gBoardWidth + 15; x++)
+		{
+			SetCurrentCursorPos(x, y);
+			printf("─");
+		}
+	}
+	for (int y = gBoardHeight / 2 - 5; y <= gBoardHeight / 2 + 5; y++)
+	{
+		for (int x = gBoardWidth - 15; x <= gBoardWidth + 15; x += 30)
+		{
+			SetCurrentCursorPos(x, y);
+			printf("│");
+		}
+	}
+
+	SetCurrentCursorPos(gBoardWidth - 15, gBoardHeight / 2 - 5);
+	printf("┌");
+	SetCurrentCursorPos(gBoardWidth + 15, gBoardHeight / 2 - 5);
+	printf("┐");
+	SetCurrentCursorPos(gBoardWidth + 15, gBoardHeight / 2 + 5);
+	printf("┘");
+	SetCurrentCursorPos(gBoardWidth - 15, gBoardHeight / 2 + 5);
+	printf("└");
+
+	SetCurrentCursorPos((gBoardWidth * 2) / 2 - 10, gBoardHeight / 2 - 3);
+	printf("G A M E   P A U S E D");
+
+	SetCurrentCursorPos((gBoardWidth * 2) / 2 - 6, gBoardHeight / 2);
+	printf("▶  RESUME");
+
+	SetCurrentCursorPos((gBoardWidth * 2) / 2 - 9, gBoardHeight / 2 + 2);
+	printf("RETURN TO MAIN MENU");
+
+
+
+	while (1)
+	{
+		if (_kbhit() != 0)
+		{
+			int key = _getch();
+
+			switch (key)
+			{
+			case DARROW:
+				if (select == 1)
+				{
+					select = 0;
+					SetCurrentCursorPos((gBoardWidth * 2) / 2 - 6, gBoardHeight / 2);
+					printf("  ");
+					SetCurrentCursorPos((gBoardWidth * 2) / 2 - 13, gBoardHeight / 2 + 2);
+					printf("▶");
+				}
+				break;
+			case UARROW:
+				if (select == 0)
+				{
+					select = 1;
+					SetCurrentCursorPos((gBoardWidth * 2) / 2 - 13, gBoardHeight / 2 + 2);
+					printf("  ");
+					SetCurrentCursorPos((gBoardWidth * 2) / 2 - 6, gBoardHeight / 2);
+					printf("▶");
+				}
+				break;
+			case ENTER:
+				return select;
+				break;
+			}
+		}
+	}
+}
+void ShowGameOver()
+{
+	for (int y = gBoardHeight / 2 - 6; y <= gBoardHeight / 2 + 6; y++)
+	{
+		for (int x = (gBoardWidth * 2) / 2 - 15; x <= (gBoardWidth * 2) / 2 + 15; x++)
+		{
+			SetCurrentCursorPos(x, y);
+			printf("  ");
+		}
+	}
+
+	for (int y = gBoardHeight / 2 - 5; y <= gBoardHeight / 2 + 5; y += 10)
+	{
+		for (int x = gBoardWidth - 15; x <= gBoardWidth + 15; x++)
+		{
+			SetCurrentCursorPos(x, y);
+			printf("─");
+		}
+	}
+	for (int y = gBoardHeight / 2 - 5; y <= gBoardHeight / 2 + 5; y++)
+	{
+		for (int x = gBoardWidth - 15; x <= gBoardWidth + 15; x += 30)
+		{
+			SetCurrentCursorPos(x, y);
+			printf("│");
+		}
+	}
+
+	SetCurrentCursorPos(gBoardWidth - 15, gBoardHeight / 2 - 5);
+	printf("┌");
+	SetCurrentCursorPos(gBoardWidth + 15, gBoardHeight / 2 - 5);
+	printf("┐");
+	SetCurrentCursorPos(gBoardWidth + 15, gBoardHeight / 2 + 5);
+	printf("┘");
+	SetCurrentCursorPos(gBoardWidth - 15, gBoardHeight / 2 + 5);
+	printf("└");
+
+	SetCurrentCursorPos((gBoardWidth * 2) / 2 - 8, gBoardHeight / 2 - 3);
+	printf("G A M E   O V E R !");
+
+	SetCurrentCursorPos((gBoardWidth * 2) / 2 - 8, gBoardHeight / 2 + 2);
+	printf("PRESS ANY KEY TO");
+	SetCurrentCursorPos((gBoardWidth * 2) / 2 - 9, gBoardHeight / 2 + 3);
+	printf("RETURN TO MAIN MENU");
+
+	Sleep(1500);
+
+	while (1)
+	{
+		if (_kbhit() != 0)
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
+void ShowStageClear(int time)
+{
+	
+}
+void ClearConsole()
+{
+	for (int y = 0; y < MAX_HEIGHT; y++)
+	{
+		for (int x = 0; x < MAX_WIDTH; x++)
+		{
+			SetCurrentCursorPos(x, y);
+			printf("  ");
+		}
+	}
+}	
